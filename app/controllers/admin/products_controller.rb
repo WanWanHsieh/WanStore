f76bebd755e2e5e36d52219980b1102dd830dbc4
WanWanHsieh <memory790225@gmail.com>
2019-05-08 19:16:59 +0800

    add add_to_cart button

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,7 +11,7 @@ class Admin::ProductsController < ApplicationController
     end
 
     def update
-        @product = Product.find(Params[:id])
+        @product = Product.find(params[:id])
         if @product.update(product_params)
             redirect_to admin_products_path
         else
